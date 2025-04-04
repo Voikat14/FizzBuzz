578c8e4a5e0b861c2b2d906a50e2c5a8ded60283
Laurin <if24b292@technikum-wien.at>
2025-04-04 10:47:15 +0200

    Wenn Zahl durch 5 Buzz ausgeben

--- main.cpp
+++ main.cpp
@@ -9,7 +9,11 @@ int main()
     {
         if(i%3==0)
         {
-            cout<<"Fizz!"<<endl;
+            cout<<"Fizz"<<endl;
+        }
+        else if(i%5==0)
+        {
+            cout<<"Buzz"<<endl;
         }
         else
         {
